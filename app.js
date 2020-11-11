4af714666f28f84c730b92f6b12a1dc014b8c338
RolandStrod <KhaleAldeAsad@gmail.com>
2020-11-11 14:48:37 +0200

    commit

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const currentImage = document.querySelector('#image');
+const images = ['img/pic1.jpeg','img/pic2.jpeg','img/pic3.jpeg','img/pic4.jpeg'];
 function changeImage(){
+
     console.log("Hello from script file!");
+    let rnd = Math.floor(Math.random() * images.length);
+    currentImage.src = images[rnd];
+    console.log(rnd);
 }
